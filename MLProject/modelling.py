135da6639cd8ef035ab0b3a9e8325e7b57bf724b
Satria <satriasauraly@gmail.com>
2025-12-23 09:20:04 +0700

    adjusting files

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,28 +1,67 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 import mlflow
 import mlflow.sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import sys
+import os
+import numpy as np
+import warnings
 
-mlflow.set_experiment("bank_marketing_rf")
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(42)
 
-with mlflow.start_run():
-    df = pd.read_csv("MLProject/bank_clean.csv")
+    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
+    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else None
+
+    file_path = (
+        sys.argv[3]
+        if len(sys.argv) > 3
+        else os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "bank_clean.csv"
+        )
+    )
+
+    df = pd.read_csv(file_path)
 
     X = df.drop("y", axis=1)
     y = df["y"]
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42, stratify=y
+        X,
+        y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
     )
 
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
+    input_example = X_train.iloc[:5]
 
-    accuracy = model.score(X_test, y_test)
+    mlflow.set_experiment("bank_marketing_rf")
 
-    mlflow.log_param("n_estimators", 100)
-    mlflow.log_metric("accuracy", accuracy)
-    mlflow.sklearn.log_model(model, "model")
+    with mlflow.start_run():
+        model = RandomForestClassifier(
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            random_state=42
+        )
 
-    print(f"Accuracy: {accuracy}")
+        model.fit(X_train, y_train)
+
+        accuracy = model.score(X_test, y_test)
+
+        # Log params & metrics
+        mlflow.log_param("n_estimators", n_estimators)
+        mlflow.log_param("max_depth", max_depth)
+        mlflow.log_metric("accuracy", accuracy)
+
+        # Log model
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",
+            input_example=input_example
+        )
+
+        print(f"Accuracy: {accuracy}")
